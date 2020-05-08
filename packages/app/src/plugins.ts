2e64f01a555a78a53982021463f4c19a1d756d34
nikolasnascimentozup <61994058+nikolasnascimentozup@users.noreply.github.com>
2020-05-08 16:06:03 -0300

    criacao template nodeserver
    criacao template nodeserver-plugin

--- packages/app/src/plugins.ts
+++ packages/app/src/plugins.ts
@@ -24,3 +24,5 @@ export { plugin as Teste } from '@backstage/plugin-teste';
 export { plugin as Omnichannel } from '@backstage/plugin-omnichannel';
 export { plugin as Nikolas } from '@backstage/plugin-nikolas';
 export { plugin as Nodeurl } from '@backstage/plugin-nodeurl';
+export { plugin as Noderul10 } from '@backstage/plugin-noderul10';
+export { plugin as Nodeserver1 } from '@backstage/plugin-nodeserver1';
